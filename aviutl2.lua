6fb0ae63e37967e35c14f7109b8d71db5052143c
karoterra <karoterra2008@gmail.com>
2025-11-02 16:16:44 +0900

    feat: beta18a で追加された機能を反映

--- aviutl2.lua
+++ aviutl2.lua
@@ -111,6 +111,7 @@ local camera_param={}
 ---@field index integer # 個別オブジェクトの番号 (0-indexed)
 ---@field num integer   # 個別オブジェクトの個数
 ---@field id integer    # オブジェクトのID
+---@field effect_id integer # オブジェクトの内の対象エフェクトのID
 ---@field track0 number # 1番目のトラックバーの値
 ---@field track1 number # 2番目のトラックバーの値
 ---@field track2 number # 3番目のトラックバーの値

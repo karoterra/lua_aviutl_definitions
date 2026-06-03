4537876c0a937074d9509b7e1c1a32c15e418568
Korarei <132204522+korarei@users.noreply.github.com>
2026-06-04 01:48:25 +0900

    fix: setfont関数の定義漏れの修正

--- aviutl2.lua
+++ aviutl2.lua
@@ -1616,6 +1616,83 @@ function SHIFT(a,b)end
 ---@nodiscard
 function rotation(x0,y0,x1,y1,x2,y2,x3,y3,zoom,r)end
 
+---@alias setfont_type
+---|0 # 標準文字
+---|1 # 影付き文字
+---|2 # 影付き文字(薄)
+---|3 # 縁取り文字
+---|4 # 縁取り文字(細)
+---|5 # 縁取り文字(太)
+---|6 # 縁取り文字(角)
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+function setfont(name,size)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+function setfont(name,size,type)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+function setfont(name,size,type,col1)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+---@param col2 integer # 影/縁色
+function setfont(name,size,type,col1,col2)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+---@param col2 integer # 影/縁色
+---@param bold boolean # 太字か？ (`true` = 太字 / `false` <デフォルト> = 通常)
+function setfont(name,size,type,col1,col2,bold)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+---@param col2 integer # 影/縁色
+---@param bold boolean # 太字か？ (`true` = 太字 / `false` <デフォルト> = 通常)
+---@param italic boolean # 斜体か？ (`true` = 斜体 / `false` <デフォルト> = 通常)
+function setfont(name,size,type,col1,col2,bold,italic)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+---@param col2 integer # 影/縁色
+---@param bold boolean # 太字か？ (`true` = 太字 / `false` <デフォルト> = 通常)
+---@param italic boolean # 斜体か？ (`true` = 斜体 / `false` <デフォルト> = 通常)
+---@param charspacing number # 文字間隔
+function setfont(name,size,type,col1,col2,bold,italic,charspacing)end
+
+---`obj.load("text")`で使うフォント(書式)を変更する
+---@param name string # フォント名
+---@param size integer # サイズ
+---@param type setfont_type # 書式
+---@param col1 integer # 文字色
+---@param col2 integer # 影/縁色
+---@param bold boolean # 太字か？ (`true` = 太字 / `false` <デフォルト> = 通常)
+---@param italic boolean # 斜体か？ (`true` = 斜体 / `false` <デフォルト> = 通常)
+---@param charspacing number # 文字間隔
+---@param linespacing number # 行間隔
+function setfont(name,size,type,col1,col2,bold,italic,charspacing,linespacing)end
+
 
 ---同じパラメータを渡せば同じ結果が返る乱数関数
 ---seed = 0
